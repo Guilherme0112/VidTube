ca7759c9c04ada5daf6c90afa732eeab7821fa95
Guilherme0112 <guimendesmen124@gmail.com>
2024-10-01 21:48:11 -0300

    Database

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/08/2024 às 02:21
+-- Tempo de geração: 02/10/2024 às 02:43
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `acai`
+-- Banco de dados: `vidtube`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `idAdmin` int(11) NOT NULL,
-  `admin` int(11) NOT NULL,
-  `inputAdmin` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `emailAdmin` varchar(50) NOT NULL,
+  `timeAdmin` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,122 +41,107 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `ajuda` (
   `idAjuda` int(11) NOT NULL,
-  `idUserAjuda` int(11) NOT NULL,
-  `titleAjuda` varchar(50) NOT NULL,
-  `ajuda` text NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `textAjuda` text DEFAULT NULL,
   `timeAjuda` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(40) NOT NULL DEFAULT 'Não Respondido'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `situacao` varchar(20) DEFAULT 'Não Respondido'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `carrinho`
+-- Estrutura para tabela `comentarios`
 --
 
-CREATE TABLE `carrinho` (
-  `idCarrinho` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `idProduto` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `comentarios` (
+  `idComment` int(11) NOT NULL,
+  `idUserComment` int(11) NOT NULL,
+  `idVideoComment` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `timeComment` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `info`
+-- Estrutura para tabela `comunidade`
 --
 
-CREATE TABLE `info` (
-  `idAddress` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `endereco` text NOT NULL,
-  `bairro` varchar(50) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `rua` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pedidos`
---
-
-CREATE TABLE `pedidos` (
-  `idPedido` int(11) NOT NULL,
-  `idProduto` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `timeProduto` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) DEFAULT 'Solicitado',
-  `ownerProduct` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `products`
---
-
-CREATE TABLE `products` (
-  `idProduct` int(11) NOT NULL,
-  `nomeProduct` varchar(50) NOT NULL,
-  `ownerProduct` int(11) NOT NULL,
-  `photoProduct` text DEFAULT NULL,
-  `descricao` text DEFAULT NULL,
-  `price` double(10,2) DEFAULT NULL,
-  `productcreate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `rajuda`
---
-
-CREATE TABLE `rajuda` (
-  `idResp` int(11) NOT NULL,
-  `idStaff` int(11) NOT NULL,
-  `resposta` text NOT NULL,
+CREATE TABLE `comunidade` (
   `idPost` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `idPostUser` int(11) DEFAULT NULL,
+  `post` varchar(50) DEFAULT NULL,
+  `descPost` text DEFAULT NULL,
+  `timePost` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `topicos`
+-- Estrutura para tabela `likes`
 --
 
-CREATE TABLE `topicos` (
-  `idTopico` int(11) NOT NULL,
-  `userAdmin` int(11) NOT NULL,
-  `titleT` varchar(50) NOT NULL,
-  `topico` text NOT NULL,
-  `timeTopico` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `likes` (
+  `idLike` int(11) NOT NULL,
+  `videoLike` int(11) DEFAULT NULL,
+  `userLike` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura para tabela `respajuda`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `respajuda` (
+  `idRespAjuda` int(11) NOT NULL,
+  `idPostAjuda` int(11) DEFAULT NULL,
+  `respAjuda` text DEFAULT NULL,
+  `timePostAjuda` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `seguir`
+--
+
+CREATE TABLE `seguir` (
+  `idSeguir` int(11) NOT NULL,
+  `idSeguindo` int(11) NOT NULL,
+  `idSeguidor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `tel` varchar(15) NOT NULL,
-  `cep` varchar(9) NOT NULL,
-  `email` varchar(55) NOT NULL,
-  `senha` varchar(16) NOT NULL,
-  `timecreate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `photoProfile` text NOT NULL DEFAULT '../assets/user.jpg'
+  `nome` varchar(20) DEFAULT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `photoProfile` varchar(60) DEFAULT '../styles/icons/user.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `videos`
+--
+
+CREATE TABLE `videos` (
+  `idVideo` int(11) NOT NULL,
+  `video` varchar(100) DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `thumb` varchar(60) DEFAULT NULL,
+  `userVideo` int(11) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Despejando dados para a tabela `users`
---
-
-INSERT INTO `users` (`id`, `nome`, `tel`, `cep`, `email`, `senha`, `timecreate`, `photoProfile`) VALUES
-(2, 'Guilherme', '(63) 99132-4404', '773-10000', 'guimendesmen124@gmail.com', '12345', '2024-08-03 23:28:09', '../assets/user.jpg');
 
 --
 -- Índices para tabelas despejadas
@@ -166,63 +151,60 @@ INSERT INTO `users` (`id`, `nome`, `tel`, `cep`, `email`, `senha`, `timecreate`,
 -- Índices de tabela `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`idAdmin`),
-  ADD KEY `admin` (`admin`);
+  ADD PRIMARY KEY (`idAdmin`);
 
 --
 -- Índices de tabela `ajuda`
 --
 ALTER TABLE `ajuda`
   ADD PRIMARY KEY (`idAjuda`),
-  ADD KEY `idUserAjuda` (`idUserAjuda`);
-
---
--- Índices de tabela `carrinho`
---
-ALTER TABLE `carrinho`
-  ADD PRIMARY KEY (`idCarrinho`),
-  ADD KEY `idProduto` (`idProduto`),
   ADD KEY `idUser` (`idUser`);
 
 --
--- Índices de tabela `info`
+-- Índices de tabela `comentarios`
 --
-ALTER TABLE `info`
-  ADD PRIMARY KEY (`idAddress`),
-  ADD KEY `idUser` (`idUser`);
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`idComment`),
+  ADD KEY `idUserComment` (`idUserComment`);
 
 --
--- Índices de tabela `pedidos`
+-- Índices de tabela `comunidade`
 --
-ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`idPedido`),
-  ADD KEY `idUser` (`idUser`),
-  ADD KEY `idProduto` (`idProduto`);
+ALTER TABLE `comunidade`
+  ADD PRIMARY KEY (`idPost`);
 
 --
--- Índices de tabela `products`
+-- Índices de tabela `likes`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`idProduct`),
-  ADD KEY `ownerProduct` (`ownerProduct`);
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`idLike`);
 
 --
--- Índices de tabela `rajuda`
+-- Índices de tabela `respajuda`
 --
-ALTER TABLE `rajuda`
-  ADD PRIMARY KEY (`idResp`);
+ALTER TABLE `respajuda`
+  ADD PRIMARY KEY (`idRespAjuda`);
 
 --
--- Índices de tabela `topicos`
+-- Índices de tabela `seguir`
 --
-ALTER TABLE `topicos`
-  ADD PRIMARY KEY (`idTopico`);
+ALTER TABLE `seguir`
+  ADD PRIMARY KEY (`idSeguir`),
+  ADD KEY `idSeguindo` (`idSeguindo`),
+  ADD KEY `idSeguidor` (`idSeguidor`);
 
 --
--- Índices de tabela `users`
+-- Índices de tabela `usuarios`
 --
-ALTER TABLE `users`
+ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`idVideo`),
+  ADD KEY `userVideo` (`userVideo`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -241,88 +223,75 @@ ALTER TABLE `ajuda`
   MODIFY `idAjuda` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `carrinho`
+-- AUTO_INCREMENT de tabela `comentarios`
 --
-ALTER TABLE `carrinho`
-  MODIFY `idCarrinho` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comentarios`
+  MODIFY `idComment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de tabela `info`
+-- AUTO_INCREMENT de tabela `comunidade`
 --
-ALTER TABLE `info`
-  MODIFY `idAddress` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comunidade`
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `pedidos`
+-- AUTO_INCREMENT de tabela `likes`
 --
-ALTER TABLE `pedidos`
-  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `likes`
+  MODIFY `idLike` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de tabela `products`
+-- AUTO_INCREMENT de tabela `respajuda`
 --
-ALTER TABLE `products`
-  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `respajuda`
+  MODIFY `idRespAjuda` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `rajuda`
+-- AUTO_INCREMENT de tabela `seguir`
 --
-ALTER TABLE `rajuda`
-  MODIFY `idResp` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `seguir`
+  MODIFY `idSeguir` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `topicos`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
-ALTER TABLE `topicos`
-  MODIFY `idTopico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
--- AUTO_INCREMENT de tabela `users`
+-- AUTO_INCREMENT de tabela `videos`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `videos`
+  MODIFY `idVideo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `admin`
---
-ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`admin`) REFERENCES `users` (`id`);
-
---
 -- Restrições para tabelas `ajuda`
 --
 ALTER TABLE `ajuda`
-  ADD CONSTRAINT `ajuda_ibfk_1` FOREIGN KEY (`idUserAjuda`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `ajuda_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `usuarios` (`id`);
 
 --
--- Restrições para tabelas `carrinho`
+-- Restrições para tabelas `comentarios`
 --
-ALTER TABLE `carrinho`
-  ADD CONSTRAINT `carrinho_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `products` (`idProduct`),
-  ADD CONSTRAINT `carrinho_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`);
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`idUserComment`) REFERENCES `usuarios` (`id`);
 
 --
--- Restrições para tabelas `info`
+-- Restrições para tabelas `seguir`
 --
-ALTER TABLE `info`
-  ADD CONSTRAINT `info_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`);
+ALTER TABLE `seguir`
+  ADD CONSTRAINT `seguir_ibfk_1` FOREIGN KEY (`idSeguindo`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `seguir_ibfk_2` FOREIGN KEY (`idSeguidor`) REFERENCES `usuarios` (`id`);
 
 --
--- Restrições para tabelas `pedidos`
+-- Restrições para tabelas `videos`
 --
-ALTER TABLE `pedidos`
-  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`idProduto`) REFERENCES `products` (`idProduct`);
-
---
--- Restrições para tabelas `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`ownerProduct`) REFERENCES `users` (`id`);
+ALTER TABLE `videos`
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`userVideo`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
